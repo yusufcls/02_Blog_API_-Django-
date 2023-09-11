a8a88c7651be2b3689a8da4a2cee63d7b15d2293
Yusuf C <yusufcfly@gmail.com>
2023-09-11 14:10:09 +0200

    Language-Verbose_Name

--- blog/models.py
+++ blog/models.py
@@ -3,19 +3,28 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(verbose_name='Kategori Adı', max_length=64)
+    
+    class Meta:
+        verbose_name = 'Blog Kategori'
+        verbose_name_plural = 'Blog Kategoriler'
     
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=64)
-    content = models.TextField(null=True, blank=True)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Kullanıcı', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='Kategori', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Başlık', max_length=64)
+    content = models.TextField(verbose_name='İçerik', null=True, blank=True)
+    created_date = models.DateTimeField(verbose_name='Oluşturulma Tarihi', auto_now_add=True)
+    updated_date = models.DateTimeField(verbose_name='Güncelleme Tarihi', auto_now=True)
+    
+        
+    class Meta:
+        verbose_name = 'Blog Yazısı'
+        verbose_name_plural = 'Blog Yazıları'
     
     def __str__(self):
         return self.category + ' / ' + self.title
